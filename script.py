5a1e29fea0f56a102bc34fffad9fc94505c9db8b
Mona Khosla <mkhosla1138@gmail.com>
2025-04-21 01:15:53 -0400

    Update script.py

--- script.py
+++ script.py
@@ -33,10 +33,12 @@ def scrape_data_point():
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("a", class_="frontpage-link standard-link")
-        data_point = "" if target_element is None else target_element.text
-        loguru.logger.info(f"Data point: {data_point}")
-        return data_point
+        most_read_section = soup.find("span", id="mostRead")
+        if most_read_section:
+            target_element = soup.find("a", class_="frontpage-link standard-link")
+            data_point = "" if target_element is None else target_element.text
+            loguru.logger.info(f"Data point: {data_point}")
+            return data_point
 
 if __name__ == "__main__":
 
